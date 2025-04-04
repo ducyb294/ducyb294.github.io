4a1dfc39a9ed1875799b9d1e06a0eec74ac8bb96
Đức Lê <ducyb2942@gmail.com>
2025-04-04 12:32:40 +0700

    update layout

--- screen-recorder/index.js
+++ screen-recorder/index.js
@@ -1,35 +1,43 @@
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const video = document.querySelector("video");
+const downloadElement = document.getElementById("download");
+const downloadBtn = document.getElementById("downloadBtn");
+
 let recorder, stream;
 
 async function startRecording() {
-  stream = await navigator.mediaDevices.getDisplayMedia({
-    video: { mediaSource: "screen" }
-  });
-  recorder = new MediaRecorder(stream);
+    stream = await navigator.mediaDevices.getDisplayMedia({
+        video: {mediaSource: "screen"}
+    });
+    recorder = new MediaRecorder(stream);
 
-  const chunks = [];
-  recorder.ondataavailable = e => chunks.push(e.data);
-  recorder.onstop = e => {
-    const completeBlob = new Blob(chunks, { type: chunks[0].type });
-    video.src = URL.createObjectURL(completeBlob);
-  };
+    const chunks = [];
+    recorder.ondataavailable = e => chunks.push(e.data);
+    recorder.onstop = e => {
+        const completeBlob = new Blob(chunks, {type: chunks[0].type});
+        const videoURL = URL.createObjectURL(completeBlob);
+        video.src = videoURL;
 
-  recorder.start();
+        downloadBtn.href = videoURL;
+        downloadElement.style.display = "block";
+    };
+
+    recorder.start();
 }
 
 start.addEventListener("click", () => {
-  start.setAttribute("disabled", true);
-  stop.removeAttribute("disabled");
+    start.setAttribute("disabled", true);
+    stop.removeAttribute("disabled");
+    downloadElement.style.display = "none";
 
-  startRecording();
+    startRecording();
 });
 
 stop.addEventListener("click", () => {
-  stop.setAttribute("disabled", true);
-  start.removeAttribute("disabled");
+    stop.setAttribute("disabled", true);
+    start.removeAttribute("disabled");
 
-  recorder.stop();
-  stream.getVideoTracks()[0].stop();
+    recorder.stop();
+    stream.getVideoTracks()[0].stop();
 });
